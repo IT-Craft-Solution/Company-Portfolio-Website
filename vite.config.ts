c4fd469e81d3c40a418c5924453767de923dc49a
Bharat <2018bharatmakwana@gmail.com>
2024-12-29 18:20:46 +0530

    base url changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Company-Portfolio-Website",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
